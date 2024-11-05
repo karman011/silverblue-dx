85d4eac8f397d35f3883b4de16503fbf26898249
karman011 <karmapolice1431@proton.me>
2024-11-05 20:41:02 +0800

    add inter font

--- systemd.sh
+++ systemd.sh
@@ -6,3 +6,6 @@ systemctl enable tailscaled.service
 systemctl enable brew-setup.service
 systemctl enable brew-upgrade.timer
 systemctl enable brew-update.timer
+systemctl enable docker.socket
+systemctl enable podman.socket
+systemctl enable dx-group.service
